366fb1fcf6997d5dab8c2d5c633ffbe9e53d1b97
Aldair <aldairgarcia803@gmail.com>
2018-11-05 18:35:06 -0500

    function

--- js/index.js
+++ js/index.js
@@ -4,3 +4,28 @@ import test from './script2.js';
 console.log('test');
 
 test();
+
+function appendImageToBody(srcURL) {
+    // write a function that will append an <img> to the body with the
+    // URL provided in the parameters
+    $('#here').append('<img src=' + srcURL + '>');
+}
+function callGiphyAPIWithSearchTerm(searchTerm) {
+  var newUrl= "https://api.giphy.com/v1/stickers/search?q=" + searchTerm + "&api_key=AIzaSyBLuDHEdEXQ08sZMu11xbaP6JJQsF4TaZg";
+    $.ajax({
+      url: newUrl,
+      method: "GET",
+      success: function(response) {
+        
+        for(var i=0;i < response.data.length ; i++){
+          var url = response.data[i].images.original.url;
+          appendImageToBody(url);
+          
+           
+        }
+       
+      console.log(response);
+      console.log(response.data.length);
+      },
+    }); 
+}
